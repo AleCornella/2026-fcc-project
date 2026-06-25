03abb4e0c7469e869a345f24c15f10be7c948d6b
Alessandro Cornella <alessandro.cornella@studenti.unitn.it>
2026-06-25 19:21:07 +0200

    add kubectl command so it download the binary in the golden image

--- scripts/installDockerAndMinikube.sh
+++ scripts/installDockerAndMinikube.sh
@@ -27,3 +27,4 @@ cd  /home/minikube
 curl -LO https://storage.googleapis.com/minikube/releases/latest/minikube_latest_amd64.deb
 sudo dpkg -i minikube_latest_amd64.deb
 echo 'alias kubectl="minikube kubectl --"' >> /home/minikube/.bashrc
+kubectl options
