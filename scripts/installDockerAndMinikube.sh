8625928b070b25b660b52ea30d80ad38a26724c6
Alessandro Cornella <alessandro.cornella@studenti.unitn.it>
2026-06-27 16:57:07 +0200

    update minikube VM creation

--- scripts/installDockerAndMinikube.sh
+++ scripts/installDockerAndMinikube.sh
@@ -3,25 +3,25 @@
 sudo apt-get update
 sudo apt-get upgrade -y
 
-# sudo install -m 0755 -d /etc/apt/keyrings
-# sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
-# sudo chmod a+r /etc/apt/keyrings/docker.asc
+sudo install -m 0755 -d /etc/apt/keyrings
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+sudo chmod a+r /etc/apt/keyrings/docker.asc
     
 
-# sudo tee /etc/apt/sources.list.d/docker.sources <<EOF
-# Types: deb
-# URIs: https://download.docker.com/linux/ubuntu
-# Suites: $(. /etc/os-release && echo "${UBUNTU_CODENAME:-$VERSION_CODENAME}")
-# Components: stable
-# Architectures: $(dpkg --print-architecture)
-# Signed-By: /etc/apt/keyrings/docker.asc
-# EOF
+sudo tee /etc/apt/sources.list.d/docker.sources <<EOF
+Types: deb
+URIs: https://download.docker.com/linux/ubuntu
+Suites: $(. /etc/os-release && echo "${UBUNTU_CODENAME:-$VERSION_CODENAME}")
+Components: stable
+Architectures: $(dpkg --print-architecture)
+Signed-By: /etc/apt/keyrings/docker.asc
+EOF
 
-# sudo apt update
-# sudo apt install -y docker-ce docker-ce-cli containerd.io
-# sudo systemctl start docker
-# sudo groupadd docker
-# sudo usermod -aG docker minikube
+sudo apt update
+sudo apt install -y docker-ce docker-ce-cli containerd.io
+sudo systemctl start docker
+sudo groupadd docker
+sudo usermod -aG docker minikube
 sudo apt-get install -y qemu-kvm libvirt-daemon-system libvirt-clients bridge-utils
 sudo adduser minikube libvirt
 sudo adduser minikube kvm
@@ -33,7 +33,9 @@ sudo dpkg -i minikube_latest_amd64.deb
 # echo 'alias kubectl="minikube kubectl --"' >> /home/minikube/.bashrc
 # minikube kubectl options
 curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
-sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
+chown minikube kubectl
+su - minikube -c "sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl"
 curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/master/scripts/get-helm-3
+chown minikube get_helm.sh
 chmod 700 get_helm.sh
-./get_helm.sh
+su - minikube -c "./get_helm.sh"
