cfcfa0055be1af858874e2ce64b47b62d04abf8e
Alessandro Cornella <alessandro.cornella@studenti.unitn.it>
2026-06-29 22:37:41 +0200

    rename file

--- scripts/deply-a.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-KUBECONFIG=./kubeconfig-alice kubectl apply -f https://raw.githubusercontent.com/GoogleCloudPlatform/microservices-demo/main/release/kubernetes-manifests.yaml
-KUBECONFIG=./kubeconfig-alice kubectl apply -f hpas/hpa-a.yaml
-KUBECONFIG=./kubeconfig-alice kubectl apply -f ingress/ingress-tenant-a.yaml
