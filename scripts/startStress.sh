d2f31b2bae6630eff7d4b4d0c6ae0923438e0a9f
Alessandro Cornella <alessandro.cornella@studenti.unitn.it>
2026-06-29 19:22:22 +0200

    deply the strees test

--- scripts/startStress.sh
+++ scripts/startStress.sh
@@ -1,3 +1,25 @@
 #/bin/bash
 
-KUBECONFIG=./kubeconfig-bob kubectl apply -f deployments/job-b.yaml
+kubectl delete job cpu-hog --ignore-not-found
+NODE_NAME=$(kubectl get nodes -o jsonpath='{.items[0].metadata.name}')
+ALLOCATABLE_RAW=$(kubectl get nodes $NODE_NAME -o jsonpath='{.status.allocatable.cpu}')
+
+if [[ $ALLOCATABLE_RAW == *m ]]; then
+    TOTAL_MILLI=${ALLOCATABLE_RAW%m}
+else
+    TOTAL_MILLI=$((ALLOCATABLE_RAW * 1000))
+fi
+
+ALLOCATED_RAW=$(kubectl describe node $NODE_NAME | awk '/Allocated resources:/,/[E|e]vents:/' | awk '$1=="cpu" {print $2}')
+
+if [[ -z "$ALLOCATED_RAW" || "$ALLOCATED_RAW" == "0" ]]; then
+    ALLOCATED_MILLI=0
+elif [[ $ALLOCATED_RAW == *m ]]; then
+    ALLOCATED_MILLI=${ALLOCATED_RAW%m}
+else
+    ALLOCATED_MILLI=$((ALLOCATED_RAW * 1000))
+fi
+
+FREE_MILLI=$((TOTAL_MILLI - ALLOCATED_MILLI))
+FREE_MILLI="${FREE_MILLI}m"
+sed "s/MAX_CPU_PLACEHOLDER/$FREE_MILLI/g" deployments/job-b.yaml | KUBECONFIG=./kubeconfig-bob kubectl apply -f -
